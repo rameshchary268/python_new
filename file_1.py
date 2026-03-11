bc9d97d649f112852d9cea0333255c1b9c712c33
rameshchary268 <rameshchary1920@gmail.com>
2026-03-11 18:05:37 +0530

    initial commit

---
+++ file_1.py
@@ -0,0 +1,20 @@
+# startup.py
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+def farewell(name):
+    print(f"Goodbye, {name}!")
+
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("hello !! git")
+    greet("Ramesh")
+    farewell("Ramesh")
+    print("Sum:", add_numbers(5, 3))
+    print("Product:", multiply_numbers(5, 3))
